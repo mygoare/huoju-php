840fb15cf605d651f07f1356434eb0a413ce5808
mygoare <mygoare@gmail.com>
2014-11-20 02:47:01 +0800

    Design: select languages

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,10 +1,17 @@
     <footer class="footer uk-clearfix">
         <div class="uk-float-left">
-        <span class="copyright uk-hidden-small"><i class="uk-icon-copyright"></i> Huoju. Built with <i class="uk-icon-heart uk-text-danger"></i></span>
-        <a class="menu" href=""><?php echo lang('footer_language') ?></a>
-        <a class="menu" href="about"><?php echo lang('footer_about') ?></a>
-        <a class="menu" href="faq"><?php echo lang('footer_faq') ?></a>
-        <a class="menu" href="terms"><?php echo lang('footer_terms') ?></a>
+        <span class="copyright uk-hidden-small"><i class="uk-icon-copyright"></i> 2014 Huoju. Built with <i class="uk-icon-heart uk-text-danger"></i></span>
+        <span class="uk-display-inline-block uk-position-relative" data-uk-dropdown="{mode:'click'}">
+        <a class="menu" href="#"><?php echo lang('footer_language') ?></a>
+            <div class="uk-dropdown uk-dropdown-up uk-dropdown-small">
+                <a class="uk-display-block select-language uk-link-muted" href="">English</a>
+                <a class="uk-display-block select-language uk-link-muted" href="">简体中文</a>
+                <a class="uk-display-block select-language uk-link-muted" href="">한국어</a>
+            </div>
+        </span>
+        <a class="menu" href="<?php echo base_url(); ?>about"><?php echo lang('footer_about') ?></a>
+        <a class="menu" href="<?php echo base_url(); ?>faq"><?php echo lang('footer_faq') ?></a>
+        <a class="menu" href="<?php echo base_url(); ?>terms"><?php echo lang('footer_terms') ?></a>
         </div>
         <div class="uk-float-right">
             <i class="uk-icon-wechat"></i>
