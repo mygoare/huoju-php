3a332ff03a24215b53433bb47daafe5e61c2a99c
mygoare <mygoare@gmail.com>
2014-11-20 23:59:18 +0800

    add caret-up icon

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -2,7 +2,7 @@
         <div class="uk-float-left">
         <span class="copyright uk-hidden-small"><i class="uk-icon-copyright"></i> 2014 Huoju. Built with <i class="uk-icon-heart uk-text-danger"></i></span>
         <span class="uk-display-inline-block uk-position-relative" data-uk-dropdown="{mode:'click'}">
-        <a class="menu" href="#"><?php echo lang('footer_language') ?></a>
+        <a class="menu" href="#"><?php echo lang('footer_language') ?> <i class="uk-icon-caret-up"></i></a>
             <div class="uk-dropdown uk-dropdown-up uk-dropdown-small">
                 <a class="uk-display-block select-language uk-link-muted" href="">English</a>
                 <a class="uk-display-block select-language uk-link-muted" href="">简体中文</a>
