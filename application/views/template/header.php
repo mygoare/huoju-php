44f4c1ab22579ffe49b264c6944b3575126f0c0d
mygoare <mygoare@gmail.com>
2014-11-20 11:48:19 +0800

    fix spell error

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -7,7 +7,7 @@
         </ul>
         <div class="uk-navbar-flip">
             <ul class="uk-navbar-nav">
-                <li><a href="<?php echo base_url(); ?>user/login">Login/Sugn up <i class="uk-icon-user"></i></a></li>
+                <li><a href="<?php echo base_url(); ?>user/login">Login/Sign up <i class="uk-icon-user"></i></a></li>
             </ul>
         </div>
     </nav>
