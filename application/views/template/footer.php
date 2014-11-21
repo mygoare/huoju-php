a52ac71defb94501ee05ec92f793c0030b657cd2
mygoare <mygoare@gmail.com>
2014-11-21 13:30:30 +0800

    fix

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,6 +1,6 @@
     <footer class="footer uk-clearfix">
         <div class="uk-float-left">
-        <span class="copyright hj-hidden-mini"><i class="uk-icon-copyright"></i> 2014 Huoju. Built with <i class="uk-icon-heart uk-text-danger"></i></span>
+        <span class="copyright uk-hidden-mini"><i class="uk-icon-copyright"></i> 2014 Huoju. Built with <i class="uk-icon-heart uk-text-danger"></i></span>
         <span class="uk-display-inline-block uk-position-relative" data-uk-dropdown="{mode:'click'}">
         <a class="menu" href="#"><?php echo lang('footer_language') ?> <i class="uk-icon-caret-up"></i></a>
             <div class="uk-dropdown uk-dropdown-up uk-dropdown-small">
