fc1f7b5adde311fdd7f2ec0db6b99c32e5035848
mygoare <mygoare@gmail.com>
2014-11-23 18:10:54 +0800

    a Test library to test

---
+++ application/libraries/Test.php
@@ -0,0 +1,11 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Test
+{
+    public function __construct()
+    {
+        define("MYGOARE", "goare mao");
+    }
+}
+
+?>
